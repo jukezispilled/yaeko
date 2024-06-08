ebee435aa24d0cfcb90a54632f12cbb473db6560
Jack Anderson <whotfisjax@gmail.com>
2024-06-08 15:06:01 -0400

    v0

--- src/App.js
+++ src/App.js
@@ -1,53 +1,25 @@
-import React, { useRef, useEffect, useState } from 'react';
-import { BackgroundGradient } from './BackgroundGradient';
-import Xlogo from './xlogo.jpg';
-import TG from './tg.png';
-import song from './song.mp3';
+import React from "react";
+import { BackgroundGradient } from "./BackgroundGradient";
+import Xlogo from "./xlogo.jpg";
+import TG from "./tg.png";
+import AudioPlayer from "react-audio-player";
+import song from "./song.mp3"; // Assuming the song.mp3 is in the public folder
 
 function App() {
-  const audioRef = useRef(null);
-  const [isPlaying, setIsPlaying] = useState(false);
-
-  useEffect(() => {
-    if (audioRef.current) {
-      audioRef.current.play();
-      setIsPlaying(true);
-    }
-  }, []);
-
-  const togglePlay = () => {
-    if (isPlaying) {
-      audioRef.current.pause();
-    } else {
-      audioRef.current.play();
-    }
-    setIsPlaying(!isPlaying);
-  };
-
   return (
     <div className="h-screen w-screen bg-zinc-950 flex flex-col justify-center items-center overflow-x-clip relative">
       <BackgroundGradient>
         <div
           className="h-[250px] w-[250px] md:h-[45vh] md:w-[45vh] bg-zinc-950 rounded-3xl"
           style={{
-            backgroundImage: 'url(/W.jpg)',
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
+            backgroundImage: "url(/W.jpg)",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
           }}
         ></div>
       </BackgroundGradient>
-      <div className="absolute top-0 left-0 mt-5 ml-5">
-        <div className="flex items-center">
-          <button
-            className="bg-[#F6D10A] text-white font-bold py-2 px-4 rounded-lg"
-            onClick={togglePlay}
-          >
-            {isPlaying ? 'Pause' : 'Play'}
-          </button>
-          <div className="text-white font-bold py-2 px-4">
-            <audio ref={audioRef} src={song} />
-          </div>
-        </div>
+      <div className="absolute top-0 left-0 mt-5 ml-5 hidden md:block">
+        <AudioPlayer src={song} autoPlay controls />
       </div>
       <div className="absolute bottom-10 right-10 flex flex-row z-30 hidden md:flex">
         <a
